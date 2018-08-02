4e9365c6478b1b27e1fec13ab4995eeeb8eb2bc5
Natatem00 <natatemcraftandotherpresent@gmail.com>
2018-08-02 20:01:52 +0300

    Change camera mode
    Changing camera mode from third person to first person(and backward)

---
+++ C#/Change_Camera_Mode/CameraPosition.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+
+public class CameraPosition : MonoBehaviour {
+
+    //variables where have stored camera rotation
+    float currentX = 0f, currentY = 0f; 
+
+    //min and max Y for camera
+    const float MAX_Y = 10f, MIN_Y = -50f, First_MAX_Y = 60, First_MIN_Y = -60;
+
+    //camera sensitivity
+    public float cinsX, cinsY;
+
+    //begin camera position
+    Vector3 begin_pos; 
+
+    //camera mode
+    bool FirstPerson;
+
+    //joystick and touch area(links request)
+    public FixedTouchField touch;
+    public Joystick joystick;
+
+    //required components
+    public GameObject player;
+    public Movement move;
+
+
+	void Start () {
+        //TODO change on links not on "GetComponent"
+        move = player.GetComponent<Movement>();
+        begin_pos = new Vector3(0, 2f, -1f);
+        cinsX = 0.2f; 
+        cinsY = 0.2f;
+        FirstPerson = false;
+	}
+
+    void Update()
+    {   //change camera mode
+        //TODO change on CHANGE_CAMERA_BUTTON
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ChangeCameraMode();
+        }
+
+        SetCurrentPositions();
+
+        //if camera in FPS mode
+        if (FirstPerson)
+        {
+            FirstPersonCamera();
+        }
+    }
+
+    private void SetCurrentPositions()
+    {
+        currentX += touch.TouchDist.x * cinsX;
+        currentY -= touch.TouchDist.y * cinsY;
+    }
+
+    void LateUpdate () {
+        //if camera in TPS mode
+        if (!FirstPerson)
+        {
+            ThirdPersonCamera();
+        }
+    }
+
+    private void ChangeCameraMode()
+    {
+        FirstPerson = !FirstPerson;
+    }
+
+    private void ThirdPersonCamera()
+    {
+        currentY = Mathf.Clamp(currentY, MIN_Y, MAX_Y);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        gameObject.transform.position = player.transform.position + rotation * begin_pos;
+        gameObject.transform.LookAt(new Vector3(player.transform.position.x + 0.2f, player.transform.position.y + 1.6f, player.transform.position.z));
+        if (joystick.Horizontal > 0)
+            move.SetRotation(new Vector3(0, currentX, 0));
+    }
+
+    private void FirstPersonCamera()
+    {
+        currentY = Mathf.Clamp(currentY, First_MIN_Y, First_MAX_Y);
+        //camera on player head
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 2f, player.transform.position.z);
+        move.SetRotation(new Vector3(0, currentX, 0));
+        transform.localRotation = Quaternion.Euler(new Vector3(currentY, currentX, 0));
+        return;
+    }
+}
