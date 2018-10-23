b07bb17330eeb65511f35bc5fbb5df1313db2d61
Natatem00 <natatemcraftandotherpresent@gmail.com>
2018-10-23 22:17:11 +0300

    C# Log manager(Unity3D)

---
+++ C#/Unity3D/Log_Manager/LogManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class LogManager : MonoBehaviour {
+
+    System.IO.StreamWriter writeToFile;
+    string logFile = "log.txt";
+
+    static LogManager logTemp = null;
+
+    void Awake()
+    {
+        if(logTemp)
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }
+
+        logTemp = this;
+    }
+
+	void Start () {
+        DontDestroyOnLoad(gameObject);
+
+        // creates file
+        writeToFile = new System.IO.StreamWriter(Application.persistentDataPath + "/" + logFile);
+	}
+
+    void OnEnable()
+    {
+        Application.logMessageReceived += HandleLog;
+    }
+
+    void OnDisable()
+    {
+        Application.logMessageReceived -= HandleLog;
+    }
+
+    void HandleLog(string logString, string stackTree, LogType type)
+    {
+        if(type == LogType.Error || type == LogType.Exception)
+        {
+            Debug.Log(Application.persistentDataPath);
+            writeToFile.WriteLine("Logged at: " + System.DateTime.Now.ToString() + " - Log Desc: "
+                + logString + " - Trace: " + stackTree + " - Type: " + type.ToString());
+        }
+    }
+
+    void OnDestroy()
+    {
+        writeToFile.Close();
+    }
+	
+}
