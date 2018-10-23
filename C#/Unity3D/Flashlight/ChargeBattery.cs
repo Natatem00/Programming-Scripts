b8782a4185498328e16e452105363279c1e803bf
Natatem00 <natatemcraftandotherpresent@gmail.com>
2018-10-23 22:12:58 +0300

    C# Pick Up Items, Hologram Shader (Unity3D)

---
+++ C#/Unity3D/Flashlight/ChargeBattery.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ChargeBattery : MonoBehaviour, Pickable {
+
+    Flashlight flashlight;
+
+    void Start()
+    {
+        flashlight = GameObject.FindGameObjectWithTag("Flashlight").GetComponent<Flashlight>();
+    }
+
+    void Pickable.PickUp()
+    {
+        flashlight.AddBattery(Random.Range(5f, 20f));
+        Destroy(gameObject);
+    }
+}
---
+++ C#/Unity3D/Grab_And_Pick_Up_Items/PickUpItems.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class GrabItems : MonoBehaviour {
+
+    [SerializeField]
+    float maxDistance = 0.8f;                 // ray max distance
+    float radius = 0.3f;                      // sphere radius
+
+    float timeToUpdate = 0.1f;     
+    float currentTime = 0f;
+
+    [SerializeField]
+    Transform playerTransform;
+
+    [SerializeField]
+    LayerMask layerMask;
+
+    RaycastHit hitObject;
+
+    bool grabbed = false;
+
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        if (currentTime >= timeToUpdate && !grabbed)            // optimization
+        {
+            FindPickableItem();
+        }
+        GrabItem();
+    }
+
+    void FindPickableItem()
+    {
+        // find pickable object
+        if (Physics.SphereCast(playerTransform.position, radius, transform.forward, out hitObject, maxDistance, layerMask, QueryTriggerInteraction.UseGlobal))
+        {
+            Debug.Log(hitObject.transform.tag);
+            Debug.Log(grabbed);
+            if (Input.GetMouseButton(0))
+            {
+                // pick up object
+                hitObject.transform.GetComponent<Pickable>().PickUp();
+            }
+            else if (Input.GetKey(KeyCode.E))
+            {
+                grabbed = true;
+            }
+        }
+        currentTime = 0f;
+    }
+
+    void GrabItem()
+    {
+        if (grabbed)
+        {
+            if (!Input.GetKey(KeyCode.E))
+            {
+                grabbed = false;
+            }
+            timeToUpdate = 0f;
+            // pick up object
+            hitObject.transform.position = playerTransform.position + transform.forward * maxDistance;
+        }
+        else
+        {
+            timeToUpdate = 0.1f;
+        }
+    }
+}
