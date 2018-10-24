f491920f091c8605a0e22f4d7fa1dfe21d3176db
Natatem00 <natatemcraftandotherpresent@gmail.com>
2018-10-25 00:12:39 +0300

    C# PickUpItem changes

--- C#/Unity3D/Grab_And_Pick_Up_Items/PickUpItems.cs
+++ C#/Unity3D/Grab_And_Pick_Up_Items/PickUpItems.cs
@@ -3,10 +3,10 @@
 public class PickUpItems : MonoBehaviour {
 
     [SerializeField]
-    float maxDistance = 1f;                   // ray max distance
-    float radius = 0.7f;                      // sphere radius
+    float maxDistance = 0.8f;                   // ray max distance
+    float radius = 0.7f;                      	// sphere radius
 
-    float timeToUpdate = 0.1f;     
+    float timeToUpdate = 0.1f;
     float currentTime = 0f;
 
     [SerializeField]
@@ -22,11 +22,18 @@ public class PickUpItems : MonoBehaviour {
     void Update()
     {
         currentTime += Time.deltaTime;
-        if (currentTime >= timeToUpdate && !grabbed)            // optimization
+        if (!grabbed && currentTime >= timeToUpdate)            // optimization
         {
             FindPickableItem();
         }
+    }
+
+    void LateUpdate()
+    {
+      if(grabbed)
+      {
         GrabItem();
+      }
     }
 
     void FindPickableItem()
@@ -35,7 +42,6 @@ public class PickUpItems : MonoBehaviour {
         if (Physics.SphereCast(playerTransform.position, radius, transform.forward, out hitObject, maxDistance, layerMask, QueryTriggerInteraction.UseGlobal))
         {
             Debug.Log(hitObject.transform.tag);
-            Debug.Log(grabbed);
             if (Input.GetMouseButton(0))
             {
                 // pick up object
@@ -51,19 +57,14 @@ public class PickUpItems : MonoBehaviour {
 
     void GrabItem()
     {
-        if (grabbed)
+        if (!Input.GetKey(KeyCode.E))
         {
-            if (!Input.GetKey(KeyCode.E))
-            {
-                grabbed = false;
-            }
-            timeToUpdate = 0f;
-            // pick up object
-            hitObject.transform.position = playerTransform.position + transform.forward * maxDistance;
+            grabbed = false;
+            hitObject.rigidbody.freezeRotation = false;
+            return;
         }
-        else
-        {
-            timeToUpdate = 0.1f;
-        }
+        // pick up object
+        hitObject.rigidbody.freezeRotation = true;
+        hitObject.transform.position = playerTransform.position + transform.forward * maxDistance;
     }
 }
