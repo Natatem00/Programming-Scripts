af26148db7c9474c064806d0d3091eff390d2376
Artem Kotrus <natatemcraftandotherpresent@gmail.com>
2019-03-25 22:35:05 +0200

    helperThread

---
+++ Unity3D/Threads/HelperThread.cs
@@ -0,0 +1,62 @@
+namespace Helpers.Thread
+{
+    using System.Threading;
+    public class HelperThread
+    {
+        bool isDone = false;
+        Thread thread = null;
+		
+		object locker = new object();
+
+
+        /// <summary>
+        /// starts the thread to processing the ThreadFunction
+        /// </summary>
+        public virtual void Start()
+        {
+            thread = new Thread(Run);
+            thread.Start();
+        }
+
+        public virtual void Aboart()
+        {
+            if (thread != null)
+            {
+                thread.Abort();
+            }
+        }
+
+        /// <summary>
+        /// thread calling this function when finished the ThreadFunction
+        /// </summary>
+        public virtual void OnFinished() { }
+        /// <summary>
+        /// function to process
+        /// </summary>
+        public virtual void ThreadFunction() { }
+        /// <summary>
+        /// checks thread status
+        /// </summary>
+        public virtual bool Update()
+        {
+            if (isDone)
+            {
+                OnFinished();
+                thread.Abort();
+                return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// thread function
+        /// </summary>
+        public virtual void Run()
+        {
+			lock(locker)
+			{
+            ThreadFunction();
+            isDone = true;
+			}
+        }
+    }
+}
